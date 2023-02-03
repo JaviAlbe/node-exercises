a7b3317f1d8030b03b7744ddedb2eea090730c3f
Javier Albeniz <j.albenizlopez@gmail.com>
2023-02-03 22:14:42 +1100

    Product Model created to move the projecto to MVC pattern

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,4 @@
-const adminData = require("../routes/admin");
-
-const products = [];
+const Product = require('../models/product.js')
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
@@ -13,11 +11,14 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    // products.push({ title: req.body.title });
+    const product = new Product(req.body.title)
+    product.save()
     res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll()
     res.render('shop', {
         prods: products,
         pageTitle: 'Shop',
